7fbab050b7e0a476969f4d294e6666b10c365f3e
Esteban Suarez Prado <estebansuarezprado420@gmail.com>
2023-11-18 18:26:47 -0400

    control flow

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
+type Grade = "A" | "B" | "C";
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './control-flow.component.html',
   styleUrl: './control-flow.component.css'
 })
 export default class ControlFlowComponent {
-
+  public showContent = signal(false);
+  public grade = signal<Grade>('A');
+  public note: number = 100;
+  public frameworks = signal(['Angular', 'Qwik', 'Svelte', 'React', 'vue'])
+  public frameworksEmpty = signal([])
+  /**
+   * toggleContent
+   */
+  public toggleContent() {
+    this.showContent.update((value) => !value)
+  }
+  public calculateNote(note: number) {
+    console.log(note)
+    switch (true) {
+      case note >= 90:
+        this.grade.update((value) => 'A')
+        break;
+      case note >= 70:
+        this.grade.update((value) => 'B')
+        break;
+      case note < 70:
+        this.grade.update((value) => 'C')
+        break;
+      default:
+        break;
+    }
+  }
 }
